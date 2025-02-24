545bf45acc14bca2b84381f3718289145254f49a
Joshua <83802311+joshua-siw@users.noreply.github.com>
2025-02-24 16:19:16 +0100

    Update dev-proxy/InactivityTimer.cs
    applied suggestion for formatting in InactivityTimer
    
    Co-authored-by: Garry Trinder <garry@trinder365.co.uk>

--- dev-proxy/InactivityTimer.cs
+++ dev-proxy/InactivityTimer.cs
@@ -2,18 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace DevProxy
+namespace DevProxy;
+
+internal class InactivityTimer(long timeoutSeconds, Action timeoutAction)
 {
-    public class InactivityTimer(long timeoutSeconds, Action timeoutAction)
-    {
-        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(timeoutSeconds);
-        private readonly Timer _timer = new(_ => timeoutAction.Invoke(), null, TimeSpan.FromSeconds(timeoutSeconds), Timeout.InfiniteTimeSpan);
-
-        public void Reset() => _timer.Change(_timeout, Timeout.InfiniteTimeSpan);
-
-        public void Stop()
-        {
-            _timer.Dispose();
-        }
-    }
+    private readonly TimeSpan _timeout = TimeSpan.FromSeconds(timeoutSeconds);
+    private readonly Timer _timer = new(_ => timeoutAction.Invoke(), null, TimeSpan.FromSeconds(timeoutSeconds), Timeout.InfiniteTimeSpan);
+    public void Reset() => _timer.Change(_timeout, Timeout.InfiniteTimeSpan);
+    public void Stop() => _timer.Dispose();
 }
