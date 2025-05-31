5628a97e654353feeb90fa890acb321c93908874
Artem Azaraev <artem.azaraev@gmail.com>
2025-05-31 17:06:41 +0300

    Fix regression, notify to update a beta version if there is a released one

--- dev-proxy/UpdateNotification.cs
+++ dev-proxy/UpdateNotification.cs
@@ -40,7 +40,7 @@ internal static class UpdateNotification
             }
 
             var latestReleaseVersion = latestRelease.Version;
-            var currentVersion = ProxyUtils.NormalizeVersion(ProxyUtils.ProductVersion);
+            var currentVersion = ProxyUtils.ProductVersion;
 
             // -1 = latest release is greater
             // 0 = versions are equal
